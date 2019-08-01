9aceb5b30c547587048a4b3ffe04e1b8a87f4348
shiyu.du <shiyu.du@daocloud.io>
2019-08-01 16:27:16 +0800

    增加学习文档

--- src/main/java/com/daocloud/acl/config/MySecurityConfig.java
+++ src/main/java/com/daocloud/acl/config/MySecurityConfig.java
@@ -19,7 +19,13 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        /**
+         * 匹配 "/" 路径，不需要权限即可访问
+         * 匹配 "/user" 及其以下所有路径，都需要 "USER" 权限
+         * 登录地址为 "/login"，登录成功默认跳转到页面 "/user"
+         * 退出登录的地址为 "/logout"，退出成功后跳转到页面 "/login"
+         * 默认启用 CSRF
+         */
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/user/**").hasRole("USER")
@@ -28,10 +34,13 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().logoutUrl("/logout").logoutSuccessUrl("/login");
     }
-
+    /**
+     * 在内存中创建一个名为 "user" 的用户，密码为 "password"，拥有 "USER" 权限
+     */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
     }
+
 }
