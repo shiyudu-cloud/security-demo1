41eecc4a37cf5017b128440b48c10e7c9f3d0f82
shiyu.du <shiyu.du@daocloud.io>
2019-08-01 14:35:13 +0800

    spring security 保护 spring boot + thyemleaf 搭建的简单web应用

---
+++ src/main/java/com/daocloud/acl/MsContentSampleApplication.java
@@ -0,0 +1,13 @@
+package com.daocloud.acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsContentSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsContentSampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/daocloud/acl/config/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.daocloud.acl.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @Author: dushiyu
+ * @Date: 2019-07-31 17:23
+ * @Version 1.0
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
